06d8308b1c8b92b71ae19fe24478abafafff1402
Vasiliki Tsami <vasiliki.tsami@scigentech.com>
2025-07-10 16:36:37 +0300

    validation Version1

---
+++ backend/validation/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Form Validation', succsess: false, errors: req.seq.errors });
+  req.session.errors = null;
+});
+
+router.post('/submit', function(res, req, next){
+  //Chech Validity
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
+
+});
+
+module.exports = router;
